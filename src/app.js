560f9527a0bac06d86bc5c5aeb4d4258537fd552
mar2393 <85914773+mar2393@users.noreply.github.com>
2021-08-14 18:36:22 -0500

    Added real time data to Temperature, Description & CIty

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 function displayTemperature(response) {
-  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  cityElement.innerHTML = response.data.name;
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
 }
 
 let apiKey = "d93c482c792cf783295525de63b6fb3a";
