7ea0d11ddf6c3a6a262c89dccc0e3f11a00f77bb
mar2393 <85914773+mar2393@users.noreply.github.com>
2021-08-14 18:56:46 -0500

    Added Wind and Humidity data

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  humidityElement.innerHTML = response.data.main.humidity;
   descriptionElement.innerHTML = response.data.weather[0].description;
   cityElement.innerHTML = response.data.name;
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
